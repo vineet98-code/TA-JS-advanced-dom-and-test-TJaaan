60c42ed44687f0118d93213477a797fd580e3efd
Vineet <vineet98love@gmail.com>
2021-08-29 15:39:43 +0530

    block-BJaajc

---
+++ block-BJaajc/code/index.js
@@ -0,0 +1,23 @@
+function getFullName(firstName, lastName){
+   return `firstName  +  lastName`
+}
+
+
+function isPalindrome(value){
+    
+}
+
+function getCircumfrence() {
+    return `The circumference is NN`;
+}
+
+function getArea() {
+    return `The area is NN`;
+}
+
+module.exports = {
+    getFullName,
+    isPalindrome,
+    getCircumfrence,
+    getArea
+};
