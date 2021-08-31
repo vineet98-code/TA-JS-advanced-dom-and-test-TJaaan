072ab00e21c9b758028bbee6e1792914cde4009c
Vineet <vineet98love@gmail.com>
2021-08-31 11:43:05 +0530

    update

--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -1,2 +1,7 @@
 const index = require("./index");
 
+test('getFullName firstName + lastName to equal `firstName  +  lastName`', () => {
+    expect(getFullName('Jon', 'Show')).toBe('Jon Show');
+});
+
+
