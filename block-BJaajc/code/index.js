8a6275ee1aa4f12c0b62ad5fc30e0d189a174691
Vineet <vineet98love@gmail.com>
2021-08-31 11:51:08 +0530

    update

--- block-BJaajc/code/index.js
+++ block-BJaajc/code/index.js
@@ -3,25 +3,25 @@ function getFullName(firstName, lastName){
 }
 
 
-function isPalindrome(value){
+// function isPalindrome(value){
     
-}
+// }
 
-function getCircumfrence() {
-    return `The circumference is NN`;
-}
+// function getCircumfrence() {
+//     return `The circumference is NN`;
+// }
 
-function getArea() {
-    return `The area is NN`;
-}
-function getArea() {
-    return `The area is NN`;
-}
+// function getArea() {
+//     return `The area is NN`;
+// }
+// function getArea() {
+//     return `The area is NN`;
+// }
 
 
 module.exports = {
     getFullName,
-    isPalindrome,
-    getCircumfrence,
-    getArea
+    // isPalindrome,
+    // getCircumfrence,
+    // getArea
 };
